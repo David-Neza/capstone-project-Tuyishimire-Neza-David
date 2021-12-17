2c0a33d0c2d132cd3a1856a5397a185a07f29aa4
Tuyishimire Neza David <tuyishmirend@gmail.com>
2021-12-17 10:13:04 +0200

    Added a dashboard that saves a users pages

---
+++ JS/dash.js
@@ -0,0 +1,56 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAS3lu2Qc6Iu9yZpstvpTR2C0Ksso-lD0c",
+    authDomain: "fir-js-2636f.firebaseapp.com",
+    databaseURL: "https://fir-js-2636f-default-rtdb.firebaseio.com",
+    projectId: "fir-js-2636f",
+    storageBucket: "fir-js-2636f.appspot.com",
+    messagingSenderId: "395682592303",
+    appId: "1:395682592303:web:1bcf7d6ff7f689566cb8da"
+};
+firebase.initializeApp(firebaseConfig);
+
+var author_name,
+    title,
+    article;
+
+var file;
+var id = 1;
+
+var ImgUrl;
+var files = [];
+var reader = new FileReader();
+
+function readForm() {
+    author_name = document.getElementById("author_name").value;
+    title = document.getElementById("title").value;
+    article = document.getElementById("article").value;
+    file = document.getElementById('files').files[0];
+    console.log(author_name);
+}
+document.getElementById("insert").onclick = function () {
+    readForm();
+
+    let storageRef = firebase.storage().ref('Articles');
+    let file = document.getElementById('files').files[0];
+    console.log(file);
+
+    let thisRef = storageRef.child(file.name);
+
+    thisRef.put(file).then(res => {
+        console.log('upload success');
+        console.log(thisRef);
+        alert("upload success");
+    }).catch(e => {
+        console.log('Error' + e);
+    })
+    storageRef.child(file.name).getDownloadURL().then(url => {
+        console.log(url)
+        firebase.database().ref('Articles/').set({
+            link: url,
+            name: author_name,
+            title: title,
+            article: article
+
+        });
+    })
+}
